b92c96f7d957dc7cb1055d1b9fe4438e397d9844
foxsportscomactivate <radhjain1@gmail.com>
2025-07-02 11:18:57 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -16,7 +16,7 @@ import sys
 
 # -- Project information -----------------------------------------------------
 
-project = 'HP Smart'
+project = 'Canon Pixma'
 
 
 # The full version, including alpha/beta/rc tags
